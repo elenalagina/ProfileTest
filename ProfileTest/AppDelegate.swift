3f2089bb175b49bcd21473e92b14ac9b596f775f
Admin <>
2020-09-03 12:14:06 +0300

    show ios logs

--- ProfileTest/AppDelegate.swift
+++ ProfileTest/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import os
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,6 +20,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		window?.makeKeyAndVisible()
 		let vc = ViewController()
 		window?.rootViewController = UINavigationController(rootViewController: vc)
+        
+        print("XXX***XXX")
+        
+                 // Log a message to the default log and default log level.
+           os_log("This is a default message.")
+        
+        print("11111111111111")
+                    
+           // Log a message to the default log and debug log level
+           os_log("This is a debug message.", log: OSLog.default, type: .debug)
+        
+        print("22222222222222")
+                    
+           // Log an error to a custom log object.
+           let customLog = OSLog(subsystem: "com.your_company.your_subsystem",
+                    category: "your_category_name")
+           os_log("An error occurred!", log: customLog, type: .error)
+        
 		
 		return true
 	}
